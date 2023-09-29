046c83168c9bffe860c9b76f2cf8762ce000ccdd
trvankiet02 <trvankiet02@gmail.com>
2023-09-29 12:59:05 +0700

    init microservices

---
+++ user-service/src/main/java/com/trvankiet/app/entity/User.java
@@ -0,0 +1,60 @@
+package com.trvankiet.app.entity;
+
+import com.trvankiet.app.constant.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true, exclude = {"credential"})
+@Data
+@Builder
+public class User extends AbstractMappedEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true, nullable = false, updatable = false)
+    private String userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "cover_image_url")
+    private String coverImageUrl;
+
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "about")
+    private String about;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    private Credential credential;
+}
