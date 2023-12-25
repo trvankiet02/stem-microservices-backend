1e159cdb2ac481163b08a9286b6dbf6f18af3cc0
trvankiet02 <trvankiet02@gmail.com>
2023-12-25 15:50:29 +0700

    complte dev

--- message-service/src/main/java/com/trvankiet/app/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trvankiet.app.repository;
-
-import com.trvankiet.app.entity.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends MongoRepository<Message, String> {
-}
