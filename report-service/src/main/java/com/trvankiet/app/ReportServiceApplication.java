4ab04f8a05304839817bbb785ade825cfa3ae81e
trvankiet02 <trvankiet02@gmail.com>
2024-06-25 22:47:45 +0700

    remove competition-service

--- report-service/src/main/java/com/trvankiet/app/ReportServiceApplication.java
+++ report-service/src/main/java/com/trvankiet/app/ReportServiceApplication.java
@@ -1,6 +1,6 @@
-package com.trvankiet;
+package com.trvankiet.app;
 
-public class Main {
+public class ReportServiceApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
